8db2da7a0ca6c9d582a6373e8d871a94ebf6b170
TaichunLin <helloleah23@gmail.com>
2022-05-10 05:12:26 +0800

    desc in content.tsx on home page

--- src/components/Leah/components/Content.tsx
+++ src/components/Leah/components/Content.tsx
@@ -12,9 +12,11 @@ export const Content = () => {
             <div className="absolute top-0 left-0 text-gray-300 text-9xl">
               “
             </div>
-            <h2 className="mb-2 text-2xl">Programming Language:</h2>
+            <h2 className="mb-3 text-2xl font-black text-gray-700/50">
+              Programming Language:
+            </h2>
             <p className="mb-2">
-              HTML5/CSS3, Javascript(ES6), Golang, TypeScript, PHP
+              HTML5 | CSS3 | Javascript(ES6) | Golang | TypeScript | PHP
             </p>
             <div className="absolute -bottom-7 -right-2 text-9xl leading-[0.2px] text-gray-300">
               ”
@@ -26,10 +28,12 @@ export const Content = () => {
             <div className="absolute top-0 left-0 text-gray-300 text-9xl">
               “
             </div>
-            <h2 className="mb-2 text-2xl">Technologies:</h2>
+            <h2 className="mb-3 text-2xl font-black text-gray-700/50">
+              Technologies:
+            </h2>
             <p className="mb-2">
-              React.js, Redis, Postman, Git, Tailwind CSS, styled-components
-              CSS, Sass/SCSS, Bootstrap, JQuery
+              React.js | Redis | Postman | Git | GitHub | Tailwind CSS |
+              styled-components CSS | Sass/SCSS | Bootstrap | JQuery
             </p>
             <div className="absolute -bottom-7 -right-2 text-9xl leading-[0.2px] text-gray-300">
               ”
@@ -41,9 +45,11 @@ export const Content = () => {
             <div className="absolute top-0 left-0 text-gray-300 text-9xl">
               “
             </div>
-            <h2 className="mb-2 text-2xl">Language:</h2>
-            <p>English - TOEIC 895 (Professional Working Proficiency),</p>
-            <p className="mb-2">French - DELF A2 (Elementary Proficiency)</p>
+            <h2 className="mb-3 text-2xl font-black text-gray-700/50">
+              Language:
+            </h2>
+            <p>✨ English - TOEIC 895 (Professional Working Proficiency),</p>
+            <p className="mb-2">✨ French - DELF A2 (Elementary Proficiency)</p>
             <div className="absolute -bottom-7 -right-2 text-9xl leading-[0.2px] text-gray-300">
               ”
             </div>
@@ -54,10 +60,13 @@ export const Content = () => {
             <div className="absolute top-0 left-0 text-gray-300 text-9xl">
               “
             </div>
-            <h2 className="mb-2 text-2xl"> Others skills:</h2>
+            <h2 className="mb-3 text-2xl font-black text-gray-700/50">
+              {' '}
+              Others skills:
+            </h2>
             <p className="mb-2">
-              Adobe Photoshop/Dreamweaver/XD, Figma,VSCode, Final Cut ProX,
-              Premiere, Project management
+              Adobe Photoshop | Adobe Dreamweaver | Adobe XD | Figma | VSCode |
+              Final Cut ProX | Premiere | Project management
             </p>
             <div className="absolute -bottom-7 -right-2 text-9xl leading-[0.2px] text-gray-300">
               ”
@@ -69,9 +78,12 @@ export const Content = () => {
             <div className="absolute top-0 left-0 text-gray-300 text-9xl">
               “
             </div>
-            <h2 className="mb-2 text-2xl">Hobbies & Interests:</h2>
+            <h2 className="mb-3 text-2xl font-black text-gray-700/50">
+              Hobbies & Interests:
+            </h2>
             <p className="mb-2">
-              HTML5/CSS3, Javascript(ES6), Golang, TypeScript, PHP
+              UX/UI Research | Challenging new things | Reading Scuba diving |
+              Photography | Freediving | and many outdoor activities
             </p>
             <div className="absolute -bottom-7 -right-2 text-9xl leading-[0.2px] text-gray-300">
               ”
